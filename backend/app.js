90765f9d322aa368efe5efb8d5fe854138c3fd23
Jérémy Salettes-Wozniak <HarmonyFidelis@gmail.com>
2021-06-02 21:11:07 +0200

    Connection base de données

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,24 @@
+//TODO: app.js Déclaration
+
+// SERVER
 const express = require('express');
-
 const app = express();
+// BASE DE DONNEES
+const mongoose = require('mongoose');
 
-app.use((req, res) => {
+//TODO: app.js Instruction
+// BASE DE DONNEES
+
+mongoose
+	.connect(
+		'mongodb+srv://AlchTech:5nAExNvXmC5Irc58@cluster0.ykpoo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+		{ useNewUrlParser: true, useUnifiedTopology: true },
+	)
+	.then(() => console.log('Connexion à MongoDB réussie !'))
+	.catch(() => console.log('Connexion à MongoDB échouée !'));
+
+// SERVER
+app.use((req, res, next) => {
 	res.json({ message: 'Votre requête a bien été reçue !' });
 });
 
