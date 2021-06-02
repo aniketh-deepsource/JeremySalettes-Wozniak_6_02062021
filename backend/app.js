0da8e21e091c49cff28b989dd1e88a9b2aae3382
Jérémy Salettes-Wozniak <HarmonyFidelis@gmail.com>
2021-06-02 22:19:02 +0200

    creation de la function signup

--- backend/app.js
+++ backend/app.js
@@ -5,10 +5,11 @@ const express = require('express');
 const app = express();
 // BASE DE DONNEES
 const mongoose = require('mongoose');
+// USER
+const userRoutes = require('./routes/user');
 
 //TODO: app.js Instruction
 // BASE DE DONNEES
-
 mongoose
 	.connect(
 		'mongodb+srv://AlchTech:5nAExNvXmC5Irc58@cluster0.ykpoo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
@@ -17,9 +18,25 @@ mongoose
 	.then(() => console.log('Connexion à MongoDB réussie !'))
 	.catch(() => console.log('Connexion à MongoDB échouée !'));
 
+// CORS Autorisation
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization',
+	);
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+	);
+	next();
+});
+
 // SERVER
 app.use((req, res, next) => {
 	res.json({ message: 'Votre requête a bien été reçue !' });
 });
 
+// USER
+app.use('/api/auth', userRoutes);
 module.exports = app;
