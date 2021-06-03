606347a25858160d3eb0936be6f3714dcd53c48e
Unknown <HarmonyFidelis@gmail.com>
2021-06-03 16:20:01 +0200

    Finalisation user.js

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,6 +1,7 @@
 //TODO: User_Controllers declaration
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 
 //TODO: User_Controllers instruction
 exports.signup = (req, res, next) => {
@@ -19,4 +20,26 @@ exports.signup = (req, res, next) => {
 		.catch(() => res.status(500));
 };
 
-exports.login = (req, res, next) => {};
+exports.login = (req, res, next) => {
+	User.findOne({ email: req.body.email })
+		.then((user) => {
+			if (!user) {
+				return res.status(401).json({ error: 'Utilisateur non trouver !' });
+			}
+			bcrypt
+				.compare(req.body.password, user.password)
+				.then((valid) => {
+					if (!valid) {
+						return res.status(401).json({ error: 'Mot de passe incorrect' });
+					}
+					res.status(200).json({
+						userId: user.id,
+						token: jwt.sign({ userId: user.id }, 'RANDOM_TOKEN_SECRET', {
+							expiresIn: '24h',
+						}),
+					});
+				})
+				.catch((error) => res.status(500).json({ error: error }));
+		})
+		.catch((error) => res.status(500).json({ error }));
+};
