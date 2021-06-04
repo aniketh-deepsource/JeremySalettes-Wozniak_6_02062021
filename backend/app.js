38a995d45efe3aa5962637f88e05d26aad1d0126
Restyled.io <commits@restyled.io>
2021-06-04 11:35:41 +0000

    Restyled by clang-format

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,4 @@
-//TODO: app.js Déclaration
+// TODO: app.js Déclaration
 
 // SERVER
 const express = require('express');
@@ -15,34 +15,34 @@ const bodyParser = require('body-parser');
 
 const sauceRoutes = require('./routes/sauce');
 
-//TODO: app.js Instruction
+// TODO: app.js Instruction
 // BASE DE DONNEES
 mongoose
-	.connect(
-		'mongodb+srv://AlchTech:5nAExNvXmC5Irc58@cluster0.ykpoo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-		{ useNewUrlParser: true, useUnifiedTopology: true },
-	)
-	.then(() => console.log('Connexion à MongoDB réussie !'))
-	.catch(() => console.log('Connexion à MongoDB échouée !'));
+    .connect(
+        'mongodb+srv://AlchTech:5nAExNvXmC5Irc58@cluster0.ykpoo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+        {useNewUrlParser : true, useUnifiedTopology : true},
+        )
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 // CORS Autorisation
 app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader(
-		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization',
-	);
-	res.setHeader(
-		'Access-Control-Allow-Methods',
-		'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-	);
-	next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization',
+  );
+  res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+  );
+  next();
 });
 
 // SERVER
-app.use((req, res, next) => {
-	res.json({ message: 'Votre requête a bien été reçue !' });
-});
+app.use(
+    (req, res,
+     next) => { res.json({message : 'Votre requête a bien été reçue !'}); });
 
 app.use(bodyParser.json());
 
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -9,29 +9,30 @@ router.post('/', sauceCtrl.createSauce);
 
 // MODIFIER ARTICLE
 router.put('/:id', (req, res, next) => {
-	Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-		.then(() => res.status(200).json({ message: 'Objet modifier' }))
-		.catch((error) => res.status(400).json({ error }));
+  Sauce.updateOne({_id : req.params.id}, {...req.body, _id : req.params.id})
+      .then(() => res.status(200).json({message : 'Objet modifier'}))
+      .catch((error) => res.status(400).json({error}));
 });
 
 // SUPPRESSION ARTICLE
 router.delete('/:id', (req, res, next) => {
-	Sauce.deleteOne({ _id: req.params.id })
-		.then(() => res.status(200).json({ messag: 'Objet supprimer' }))
-		.catch((error) => res.status(400).json({ error }));
+  Sauce.deleteOne({_id : req.params.id})
+      .then(() => res.status(200).json({messag : 'Objet supprimer'}))
+      .catch((error) => res.status(400).json({error}));
 });
 
 // RECUPERER ID PARTICULIER
 router.get('/:id', (req, res, next) => {
-	Sauce.find({ _id: req.params.id }).then((sauce) =>
-		res.status(200).json(sauce),
-	);
+  Sauce.find({_id : req.params.id})
+      .then(
+          (sauce) => res.status(200).json(sauce),
+      );
 });
 
 router.get('/', (req, res, next) => {
-	Sauce.find()
-		.then((sauces) => res.status(200).json(things))
-		.catch((error) => res.status(400).json({ error }));
+  Sauce.find()
+      .then((sauces) => res.status(200).json(things))
+      .catch((error) => res.status(400).json({error}));
 });
 
 module.exports = router;
