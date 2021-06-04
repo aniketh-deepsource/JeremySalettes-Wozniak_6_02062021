c480d461b00fd844ea6fc22a85496fcd8c58c1aa
Restyled.io <commits@restyled.io>
2021-06-04 11:35:46 +0000

    Restyled by prettier

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -3,7 +3,8 @@ exports.createSauce = (req, res, next) => {
   const sauce = new Sauce({
     ...req.body,
   });
-  thing.save()
-      .then(() => res.status(201).json({message : 'Objet creer'}))
-      .catch((error) => res.status(400).json({error}));
+  thing
+    .save()
+    .then(() => res.status(201).json({ message: "Objet creer" }))
+    .catch((error) => res.status(400).json({ error }));
 };
